2658de465b815bdd326fe9eac1624fc24ee360c5
Chuck Smith <chuck@example.com>
2025-10-16 14:48:25 -0400

    Hero

---
+++ src/components/Hero.jsx
@@ -0,0 +1,17 @@
+const Hero = () => {
+  return (
+    <section id="hero">
+        <div>
+            <h1>Macbook Pro</h1>
+            <img src="/title.png" alt="Macbook Title" />
+
+            <video src="/videos/hero.mp4" autoPlay muted playsInline />
+
+            <button>Buy</button>
+            <p>From $1599 or $133/mo for 12 months</p>
+        </div>
+    </section>
+  )
+}
+
+export default Hero
